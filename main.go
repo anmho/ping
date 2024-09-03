1e5923409cf766052d770deadc52d7e56fbef84d
Andrew Ho <87453706+anmho@users.noreply.github.com>
2024-09-02 18:02:42 -0700

    change ping message

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("world 2"))
+		_, err := w.Write([]byte("world 3"))
 		if err != nil {
 			return
 		}
